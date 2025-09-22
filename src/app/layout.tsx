c01148273b3dc082a31c296aba9165a33b6259ea
Loona Riviere <loona.riviere@theodo.com>
2025-09-22 17:19:33 +0200

    feat: update icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,16 @@ export const metadata: Metadata = {
     description: "By Loona & Julien",
     manifest: "/manifest.json",
     icons: {
-        icon: "/wine.svg",
-        apple: "/apple-touch-icon.png",
+        icon: [
+            { url: "/favicon.ico" },
+            { url: "/pwa/icon-192.png", sizes: "192x192", type: "image/png" },
+            { url: "/pwa/icon-512.png", sizes: "512x512", type: "image/png" },
+            { url: "/wine-heart.svg", type: "image/svg+xml" },
+        ],
+        apple: "/apple-touch-icon.png", // ← ce fichier DOIT exister
     },
+
+
     appleWebApp: {
         capable: true,
         statusBarStyle: "default",
